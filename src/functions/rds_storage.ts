a754f5a3fa70d0bf30f7e3824840a65c30be4c5f
Mike Heffner <mike@librato.com>
2019-07-23 14:10:08 -0400

    Must initialize context.

--- src/functions/rds_storage.ts
+++ src/functions/rds_storage.ts
@@ -3,6 +3,7 @@ import { RDSStorage, _rds_storage_type_str_to_type } from "../models/rds_storage
 import { RDSStorageSettingsValidator } from "../settings/rds_storage_settings_validator";
 import { RDSStoragePrice } from "../rds_storage_price";
 import { PriceDuration } from "../price_converter";
+import { _initContext } from "../context";
 
 function _rds_storage(settings: InvocationSettings, volumeType: RDSStorage, volumeSize: string|number) {
     if (!volumeSize) {
@@ -63,6 +64,8 @@ export function RDS_STORAGE_GB(settingsRange: Array<Array<string>>, volumeType: 
 export function RDS_STORAGE_GB(volumeType: string, volumeSize: string | number, region: string): number
 
 export function RDS_STORAGE_GB(settingsOrType, typeOrSize, sizeOrRegion, region?: string): number {
+    _initContext()
+
     if (!settingsOrType) {
         throw `Must specify a parameter`
     }
