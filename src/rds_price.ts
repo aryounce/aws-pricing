16fd2ab927000ca30190c318c73e9d28976782de
wimdenherder <wimdenherder@gmail.com>
2023-01-10 20:26:57 +0100

    change in path of EBS: added region to path in new cdn

--- src/rds_price.ts
+++ src/rds_price.ts
@@ -23,8 +23,8 @@ export class RDSPrice {
             throw `The No-Upfront payment option is not supported for 3 year RDS RIs`
         }
 
-        let pricePath = Utilities.formatString("/pricing/1.0/rds/%s/%s/%sindex.json",
-            this.dbEngineUrlParam(), this.purchaseTypeUrlParam(), this.azUrlParam())
+        let pricePath = Utilities.formatString("/pricing/1.0/rds/region/%s/%s/%s/%s/index.json",
+           this.settings.get('region'), this.dbEngineUrlParam(), this.purchaseTypeUrlParam(), this.azUrlParam());
 
         let body = Context.ctxt().awsDataLoader.loadPath(pricePath, this.transformPayload)
 
